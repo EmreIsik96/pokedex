1f94ab6fdb5f2bcad92f710b3cb6bf833c58a713
Emre Isik <emre-isik@web.de>
2024-07-26 23:13:37 +0200

    update code with new functions and new css attributes and bug fixed

--- showFilteredDataTemp.js
+++ showFilteredDataTemp.js
@@ -1,20 +1,47 @@
-function showFilteredData()
-{
+function showFilteredData() {
+    let content = document.getElementById("content");
+    content.innerHTML = "";
     for (let j = 0; j < filteredData.length; j++) {
         let pokemonID = filteredData[j]["id"];
         let pokemonImage = filteredData[j]["sprites"]["other"]["home"]["front_default"];
         let pokemonName = capitalizeFirstLetter(filteredData[j]["name"]);
         let typesCheckPokemon = filteredData[j]["types"];
-        let pokemonType1 = filteredData[j]["types"][0]["type"]["name"];
-        let pokemonType2 = checkType(typesCheckPokemon, j);
-       
-        content.innerHTML += `<div class="card ${pokemonType1}">
-                                    <div class=nameOfPokemon>#${pokemonID} ${pokemonName}</div>
-                                    <div> <img class="mainImagesOfPokemon" src="${pokemonImage}"></div>  
-                                    <div class="typeMainContainer">
-                                        <div class="type1Class">${pokemonType1}</div>
-                                        <div id="type2">${pokemonType2}</div>
-                                    </div>
-                            </div>`;
-        }
+        let pokemonType1 = typesCheckPokemon[0]["type"]["name"];
+        let pokemonType2 = checkType(typesCheckPokemon);
+
+        // POKEMON STATS //
+        let pokemonStatsHp = filteredData[j]["stats"][0]["base_stat"];
+        let pokemonStatsAttack = filteredData[j]["stats"][1]["base_stat"];
+        let pokemonStatsDefense = filteredData[j]["stats"][2]["base_stat"];
+        let pokemonStatsSpecialAttack = filteredData[j]["stats"][3]["base_stat"];
+        let pokemonStatsSpecialDefense = filteredData[j]["stats"][4]["base_stat"];
+        let pokemonStatsSpecialSpeed = filteredData[j]["stats"][5]["base_stat"];
+
+        content.innerHTML += `<div onclick="showAttributes(
+        '${pokemonType1}',
+        '${pokemonID}',
+        '${pokemonName}',
+        '${pokemonImage}',
+        '${pokemonStatsHp}',
+        '${pokemonStatsAttack}',
+        '${pokemonStatsDefense}',
+        '${pokemonStatsSpecialAttack}',
+        '${pokemonStatsSpecialDefense}',
+        '${pokemonStatsSpecialSpeed}')" class="card ${pokemonType1}">
+                                      <div class=nameOfPokemon>#${pokemonID} ${pokemonName}</div>
+                                      <div> <img class="mainImagesOfPokemon" src="${pokemonImage}"></div>    
+                                      <div class="typeMainContainer">
+                                          <div class="type1Class">${pokemonType1}</div>
+                                          <div>${pokemonType2}</div>
+                                      </div>
+                              </div>`;
+    }
+}
+
+function checkType(typesCheckPokemon) {
+    let pokemonType2 = "";
+    if (typesCheckPokemon.length > 1) {
+        pokemonType2 = `<div class="type2Class">${typesCheckPokemon[1]["type"]["name"]}</div>`;
+    }
+    return pokemonType2;
 }
